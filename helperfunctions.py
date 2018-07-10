74fa6855186733f96e9438f4ad2be329e4a662d4
Rich Julien <40372942+richjulien3@users.noreply.github.com>
2018-07-10 08:13:55 -0700

    hw 9 .py file

---
+++ helperfunctions.py
@@ -0,0 +1,23 @@
+
+def sum2(x,y):
+    print('The sum of', x,'and',y, 'is: ')
+    sumNum = x + y
+    return sumNum
+
+def diff2(x,y):
+    print('The difference of', x,'and',y, 'is: ')
+    diffNum = x - y
+    return diffNum
+
+def prod2(x,y):
+    print('The product of', x,'and',y, 'is: ')
+    prodNum = x * y
+    return prodNum
+
+def max2(x,y):
+    print('The max of', x,'and',y, 'is: ')
+    maxNum = max(x, y)
+    return maxNum
+
+
+### END CODE
